f4595f8b79b7527e8b69ede880b963b55593fa7d
tuyisengepaul <tuyisengepaul200@gmail.com>
2019-10-01 20:37:48 +0200

    Changing Font to make consistency adn removing usertype

--- UI/Js/login.js
+++ UI/Js/login.js
@@ -1,35 +1,27 @@
-let signInDivision=document.querySelector(".login-details");
-let signUpBtn=document.querySelector(".sign-up-btn");
-let signUpDivision=document.querySelector(".sign-up-details");
-let signInBtn=document.querySelector(".sign-in-btn");
-let LogInBtn=document.querySelector(".login-btn");
+/* eslint-disable eqeqeq */
+let signInDivision = document.querySelector('.login-details');
+let signUpBtn = document.querySelector('.sign-up-btn');
+let signUpDivision = document.querySelector('.sign-up-details');
+let signInBtn = document.querySelector('.sign-in-btn');
+let LogInBtn = document.querySelector('.login-btn');
 
 signUpDivision.setAttribute('class', 'hide');
 
-signInBtn.addEventListener('click', function(){
-signInDivision.setAttribute('class','login-details');
-signUpDivision.setAttribute('class','hide');
+signInBtn.addEventListener('click', () => {
+  signInDivision.setAttribute('class', 'login-details');
+  signUpDivision.setAttribute('class', 'hide');
 });
-signUpBtn.addEventListener('click', function(){
-signUpDivision.setAttribute('class', 'sign-up-details');
-signInDivision.setAttribute('class', 'hide');
+signUpBtn.addEventListener('click', () => {
+  signUpDivision.setAttribute('class', 'sign-up-details');
+  signInDivision.setAttribute('class', 'hide');
 });
-// Click Sign In Button to LogIn.
-LogInBtn.addEventListener('click', function(){
-let selectElement = document.querySelector('#user').selectedIndex;
-let user=document.getElementById("user").options;
-let userType=user[selectElement].text;
-if(userType=="Admin")
-{
-  
-  window.location="admin.html";
 
-}
-
-else
-{
-
-window.location="employee.html";
-}
-
+LogInBtn.addEventListener('click', () => {
+  let userEmail = document.getElementsByClassName('username')[0].value;
+  let userPassword = document.getElementsByClassName('userpassword')[0].value;
+  if (userEmail == 'Admin@teamwork.com' && userPassword === 'Admin') {
+    window.location = 'admin.html';
+  } else {
+    window.location = 'employee.html';
+  }
 });
