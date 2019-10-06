0fed4223d729e827c61d7c7f0a8da1bc576dd159
tuyisengepaul <tuyisengepaul200@gmail.com>
2019-10-06 12:17:52 +0200

    Chore: Adding Swagger docummentation [Finish #168970924]

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from '../swagger';
 import usersRouter from './routes/users';
 import articleRouter from './routes/articles';
 import comment from './routes/comments';
 
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1', usersRouter);
 app.use('/api/v1', articleRouter);
@@ -17,6 +19,7 @@ app.all('/', (_req, res) => {
   message += 'The goal of this application is to facilitate more interaction between colleagues and facilitate team bonding.';
   res.status(200).send({ message });
 });
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use((_req, _res, next) => {
   const error = new Error('Bad url');
