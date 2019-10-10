d6e9fb7304510a5d150ddd07765ebcff0e5f2223
tuyisengepaul <tuyisengepaul200@gmail.com>
2019-10-10 15:24:07 +0200

    Bug: Adding Validations, trimming of payloads [Finish 169064789]

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -2,14 +2,14 @@ import Joi from '@hapi/joi';
 
 const userSignup = Joi.object().keys({
 
-  firstName: Joi.string().alphanum().required(),
-  lastName: Joi.string().required(),
+  firstName: Joi.string().alphanum().trim().required(),
+  lastName: Joi.string().alphanum().trim().required(),
   email: Joi.string().email({ minDomainSegments: 2 }),
   password: Joi.string().regex(/^((?=.*[a-z])(?=.*[A-Z]))(?=.*[0-9])(?=.{8,})/).required(),
-  gender: Joi.string().required(),
-  jobRole: Joi.string().required(),
+  gender: Joi.string().valid('Male', 'M', 'Female', 'F').required(),
+  jobRole: Joi.string().trim().required(),
   department: Joi.string().required(),
-  address: Joi.string().required(),
+  address: Joi.string().trim().required(),
 });
 
 const userSignin = Joi.object().keys({
@@ -19,12 +19,12 @@ const userSignin = Joi.object().keys({
 });
 
 const article = Joi.object().keys({
-  title: Joi.string().required(),
-  article: Joi.string().required(),
+  title: Joi.string().trim().required(),
+  article: Joi.string().trim().required(),
 });
 
 const comment = Joi.object().keys({
-  comment: Joi.string().required(),
+  comment: Joi.string().trim().required(),
 });
 
 const parameter = Joi.object().keys({
